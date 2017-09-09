aca9089f546eac7c2dc5c7599c336af8abb36fc1
雨幕 <ChitoseRaame@users.noreply.github.com>
2017-09-09 16:23:45 +0900

    First Release
    资源+源代码

---
+++ MCMOD/Push.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	
+	$("html").append("<div style=\"position:absolute;width:100%;top:30%;\"><div style=\"width:500px;line-height:200px; height:200px; margin:0 auto; text-align:center\">欢迎回来</div></div>");
+
+	//你想问怎么调用么？
+	//这里中间出现一个小框 然后提示 欢迎回来
+	//ok 试试看~
+	//没有调用怎么试试看233
+});
